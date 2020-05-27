612a4316dab53d3ce36d295b8a461cace0b45064
Daniela <danielaoancea2009@yahoo.ca>
2020-05-27 18:21:44 +0000

    text

--- countOnly.js
+++ countOnly.js
@@ -1,7 +1,37 @@
-const assertEqual = function(actual, expected) {
-  return (actual + expected);
-};
 
-// TEST CODE
-console.assert(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.assert(assertEqual(1, 1));
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {}
+  for (const item of allItems) {//loops for the firstName array
+    console.log(item);
+    if(itemsToCount[item]) { //if object has key name as firstName array
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+  };
+
+  return results;
+}
+
+  const firstNames = [
+    "Karl",
+    "Salima",
+    "Agouhanna",
+    "Fang",
+    "Kavith",
+    "Jason",
+    "Salima",
+    "Fang",
+    "Joe"
+  ];
+  
+  const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+  
+  assertEqual(result1["Jason"], 1);
+  assertEqual(result1["Karima"], undefined);
+  assertEqual(result1["Fang"], 2);
+
+
+}
