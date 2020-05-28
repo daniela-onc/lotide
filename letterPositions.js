7cc29049ffe9d5c1fefff75abc1c09f183ec5af3
Daniela <danielaoancea2009@yahoo.ca>
2020-05-28 00:35:52 +0000

    text

--- letterPositions.js
+++ letterPositions.js
@@ -1,8 +1,8 @@
 const eqArrays = function (arrey1, arrey2) {
-  if (arrey1.length !== arrey2.length) {
+  if (arrey1.length !== arrey2.length) { //if arrays are of different length, return false
     return false;
   } else {
-  for (let i = 0; i < arrey1.length; i ++) {
+  for (let i = 0; i < arrey1.length; i ++) { //return false if both arrays differ by an element
     if (arrey1[i] !== arrey2[i]) {
       return false;
     }
@@ -10,7 +10,6 @@ const eqArrays = function (arrey1, arrey2) {
   return true;
 }
 };
-
 const assertArraysEqual = function (arrey1, arrey2) {
   if (!eqArrays(arrey1, arrey2)) {
     console.log(`Assertion Failed: [${arrey1}] === [${arrey2}]`);
@@ -25,15 +24,18 @@ const letterPositions = function(sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++) {
 if (sentence[i] !== " ") {
-  if (results[sentence[i]].push(i);
-    } else {
+  if (results[sentence[i]]) {
+    results[sentence[i]].push(i);
+     } else {
       results[sentence[i]] = [i];
     }
   }
 }
   return results;
+
 };
 
 const result = letterPositions("hello world");
-assertArraysEqual(result['h'], [0]);
-assertArraysEqual(result['l'], [2,3,9]);
+assertArraysEqual(result['h'], [0]); //pass
+assertArraysEqual(result['l'], [2,3,9]); //pass
+assertArraysEqual(result['e'], [8,9,10]); //fail
