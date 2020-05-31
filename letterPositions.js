0fe8dd9bd4ca9054e456253b17aa0ceede65ce12
Daniela <danielaoancea2009@yahoo.ca>
2020-05-31 00:24:58 -0400

    Update letterPositions.js

--- letterPositions.js
+++ letterPositions.js
@@ -1,41 +1,44 @@
-const eqArrays = function (arrey1, arrey2) {
+const eqArrays = function(arrey1, arrey2) {
   if (arrey1.length !== arrey2.length) { //if arrays are of different length, return false
     return false;
   } else {
-  for (let i = 0; i < arrey1.length; i ++) { //return false if both arrays differ by an element
-    if (arrey1[i] !== arrey2[i]) {
-      return false;
+    for (let i = 0; i < arrey1.length; i ++) { //return false if both arrays differ by an element
+      if (arrey1[i] !== arrey2[i]) {
+        return false;
+      }
     }
+    return true;
   }
-  return true;
-}
 };
-const assertArraysEqual = function (arrey1, arrey2) {
-  if (!eqArrays(arrey1, arrey2)) {
-    console.log(`Assertion Failed: [${arrey1}] === [${arrey2}]`);
+const assertArraysEqual = function(arrey1, arrey2) {
+  if (!eqArrays(arrey1, arrey2)) { //or if arraey1 !== array2
+    console.log(`🛑🛑🛑 Assertion Failed: [${arrey1}] !== [${arrey2}]`);
   } else {
-    console.log(`Assertion Passed: [${arrey1}] === [${arrey2}]`);
+    console.log(`✅✅✅ Assertion Passed: [${arrey1}] === [${arrey2}]`);
   }
 };
 
+//function letterPosition return all the indices (zero-based positions) in the string where each character is found
+//For each letter, instead of returning just one number to represent its number of occurrences, multiple numbers may be needed to represent all the places in the string that it shows up.
+const letterPositions = function(sentence) { //define function letterPosition
 
-//CODE HERE
-const letterPositions = function(sentence) {
-  const results = {};
-  for (let i = 0; i < sentence.length; i++) {
-if (sentence[i] !== " ") {
-  if (results[sentence[i]]) {
-    results[sentence[i]].push(i);
-     } else {
-      results[sentence[i]] = [i];
+  const result = {};  //result is an object
+ 
+  for (let letter of sentence) {
+    result[letter] = [];
+    for (let i = 0; i < sentence.length; i++) {
+      if (letter === sentence[i]) {
+        result[letter].push(i);
+      }
     }
   }
-}
-  return results;
+  return result; ////return result is an object
 
 };
 
-const result = letterPositions("hello world");
+const result = letterPositions("hello");
 assertArraysEqual(result['h'], [0]); //pass
-assertArraysEqual(result['l'], [2,3,9]); //pass
+assertArraysEqual(result['l'], [2,3,9]); //fail
 assertArraysEqual(result['e'], [8,9,10]); //fail
+
+assertArraysEqual(letterPositions("hello").e, [1]);
