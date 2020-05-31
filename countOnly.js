8be2352b66e0ec10513db035e8004a7603347e38
Daniela <danielaoancea2009@yahoo.ca>
2020-05-30 22:49:52 -0400

    Update countOnly.js

--- countOnly.js
+++ countOnly.js
@@ -1,27 +1,34 @@
 const assertEqual = function(actual, expected) {
-  return (actual + expected);
-};
-
-// TEST CODE
-console.assert(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.assert(assertEqual(1, 1));
-
-const countOnly = function(allItems, itemsToCount) {
-  const results ={}
-  for (const item of allItems) {
-    console.log(item);
-    if (itemsToCount[item]) {
-    if (results[item]) {
-      results[item] +- 1;
-    } else {
-      results[item] = 1;
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === {expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== {expected}`);
   }
-}
-    return results;
 };
 
-}
-const firstNames = [
+console.assert(assertEqual("Lighthouse Labs", "Bootcamp")); //failed
+console.assert(assertEqual(1, 1)); //pass
+
+
+//countOnly will be given an array and an object. It will return an object containing counts of everything that the input object listed. function report back #of instancres of each stringfound in allItems array of string
+
+const countOnly = function(allItems, itemsToCount) { //declare the function countOnly
+  const results = {};  //new empty object named result
+
+  for (const item of allItems) { //for..of loop to scan the entire list of names
+    console.log(item);     //print the result of for..of loop to the console
+    if (itemsToCount[item]) { //if condition to only increment our count in object results IF the item is found in the itemsToCount object
+      if (results[item]) {
+        results[item] += 1; //increment the counter for each item      result[item]) = result[item] + 1
+      } else {
+        results[item] = 1;
+      }
+    }
+    return results; //return object named result
+  }
+
+};
+const firstNames = [ //initial test code
   "Karl",
   "Salima",
   "Agouhanna",
